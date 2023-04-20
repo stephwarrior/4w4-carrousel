32f0fd94a4f4c94f8e966c9d82d3d1fc43a56444
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-04-19 21:57:53 -0400

    s11c1 création des img du carrousel

--- carrousel.php
+++ carrousel.php
@@ -23,32 +23,32 @@ wp_enqueue_scripts // le hook qui permettra d'enfiler le css et le script
 
     function enfiler_script_css()
     {
-        $version_css = filemtime(plugin_dir_path(__FILE__) . 'style.css');
-        $version_js = filemtime(plugin_dir_path(__FILE__) . 'js/carrousel.js');
-        wp_enqueue_style('style_carrousel', 
-                         plugin_dir_url(__FILE__) . 'style.css',
-                         array(),  
-                         $version_css);
-        wp_enqueue_script('js_carrousel',
-                         plugin_dir_url(__FILE__) . 'js/carrousel.js',
-                         array(),
-                         $version_js,
-                         true
+       $version_css =  filemtime(plugin_dir_path(__FILE__) . 'style.css');
+       $version_js = filemtime(plugin_dir_path(__FILE__) . 'js/carrousel.js');
+       wp_enqueue_style('style_carrousel',
+            plugin_dir_url(__FILE__) . 'style.css',
+            array(),
+            $version_css
     );
-
-    }
-
-    add_action('wp_enqueue_scripts', 'enfiler_script_css');
-
+        wp_enqueue_script('js_carrousel',
+                plugin_dir_url(__FILE__) . 'js/carrousel.js',
+                array(),
+                $version_js,
+                true
+        );
     
-    function genere_boite(){
-        return ' <button class="carrousel__ouvrir">Ouvrir Caroussel</button>
-        <div class="carrousel">
-        <button class="carrousel__x">X</button>
-        <figure class="carrousel__figure"></figure>;
-        <form class="carroussel__form"></form>
-        </div>';
     }
-    add_shortcode('carrousel','genere_boite');
+    add_action('wp_enqueue_scripts', 'enfiler_script_css' );
+    
+    function genere_boite()
+    {
+        return '<button class="carrousel__ouvrir">Ouvrir Carrousel</button>
+                <div class="carrousel">
+                    <button class="carrousel__x">X</button>
+                    <figure class="carrousel__figure"></figure>
+                    <form class="carrousel__form"></form>
+                </div>';
+    }
+    add_shortcode('carrousel', 'genere_boite');
 
 ?>
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,13 +1,45 @@
 (function(){
-    console.log("Début du carrousel")
-    let boutcarrousel__ouvrir = document.querySelector('.carrousel__ouvrir');
-    let carrousel = document.querySelector('.carrousel');
-    let carrousel__x = document.querySelector('.carrousel__x');
-    boutcarrousel__ouvrir.addEventListener('mousedown',function(){
-        carrousel.classList.add('carrousel--ouvrir')
-    })
-    carrousel__x.addEventListener('mousedown',function(){
-        carrousel.classList.remove('carrousel--ouvrir')
-    })
-
-})()
+    console.log('Début du carrousel')
+   let bouton = document.querySelector(".carrousel__ouvrir")
+   /* -------------------------------------------------------- Variable du carrousel */
+   let carrousel  = document.querySelector(".carrousel")
+   let carrousel__x = document.querySelector(".carrousel__x")
+   let carrousel__figure = document.querySelector(".carrousel__figure")
+   
+   /* -------------------------------------------------------- Variable de la galerie */
+   let galerie = document.querySelector(".galerie")
+   let galerie__img = galerie.querySelectorAll("img")
+   // console.log("galerie__img: " + galerie__img.length)
+   // console.log(carrousel.tagName)
+   
+   
+   /* ----------------------------------------------------  ouvrir boîte modale */
+   bouton.addEventListener('mousedown', function(){
+       console.log('ouvrir la boîte modale')
+       carrousel.classList.add('carrousel--activer')
+       ajouter_img_dans_carrousel()
+   
+   })
+   /* ----------------------------------------------------  fermer boîte modale */
+   carrousel__x.addEventListener('mousedown', function(){
+       console.log('fermer la boîte modale')
+       carrousel.classList.remove('carrousel--activer')
+   
+   })
+   /** 
+    * ajouter_img_dans_carrousel
+    * Ajouter l'ensemble des images de la galerie dans la boîte modale carrousel
+    */
+   function ajouter_img_dans_carrousel()
+   {
+     for (const elm of galerie__img)
+     {
+       //console.log(elm.getAttribute('src'))
+       let img = document.createElement('img')
+       img.setAttribute('src', elm.getAttribute('src'))
+       console.log (img.getAttribute('src'))
+       carrousel__figure.appendChild(img)
+     }
+   }
+   
+   })()
