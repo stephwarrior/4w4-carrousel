4730f1b66f4350f088b4e8e21a7ddbe266b7c1f1
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-05-01 02:11:30 -0400

    Ajout des boutons suivant et précédent

--- carrousel.php
+++ carrousel.php
@@ -1,16 +1,17 @@
 <?php
-    /** 
-     * Plugin Name: Carrousel
-     * Author: Stephanie Guerrier
-     * Author URI: https://github.com/stephwarrior
-     * Description: Permet d'afficher les images d'une galerie dans une boite modale navigable
-     */
 
-    //  style.css
-    // carrousel.js
-    // boite modale qui contiendra le css
+/** 
+ * Plugin Name: Carrousel
+ * Author: Stephanie Guerrier
+ * Author URI: https://github.com/stephwarrior
+ * Description: Permet d'afficher les images d'une galerie dans une boite modale navigable
+ */
 
-    /*
+//  style.css
+// carrousel.js
+// boite modale qui contiendra le css
+
+/*
     filemtime() // retourne en milliseconde le temps de la dernière sauvegarde
 plugin_dir_path() // retourne le chemin du répertoire du plugin
 __FILE__ // une constante contenant le chemin du fichier en train de s'exécuter
@@ -21,34 +22,37 @@ wp_enqueue_scripts // le hook qui permettra d'enfiler le css et le script
     
     */
 
-    function enfiler_script_css()
-    {
-       $version_css =  filemtime(plugin_dir_path(__FILE__) . 'style.css');
-       $version_js = filemtime(plugin_dir_path(__FILE__) . 'js/carrousel.js');
-       wp_enqueue_style('style_carrousel',
-            plugin_dir_url(__FILE__) . 'style.css',
-            array(),
-            $version_css
+function enfiler_script_css()
+{
+    $version_css =  filemtime(plugin_dir_path(__FILE__) . 'style.css');
+    $version_js = filemtime(plugin_dir_path(__FILE__) . 'js/carrousel.js');
+    wp_enqueue_style(
+        'style_carrousel',
+        plugin_dir_url(__FILE__) . 'style.css',
+        array(),
+        $version_css
     );
-        wp_enqueue_script('js_carrousel',
-                plugin_dir_url(__FILE__) . 'js/carrousel.js',
-                array(),
-                $version_js,
-                true // ajoute le script carrousel.js à la fin de la page
-        );
-    
-    }
-    add_action('wp_enqueue_scripts', 'enfiler_script_css' );
-    
-    function genere_boite()
-    {
-        return '<button class="carrousel__ouvrir">Ouvrir Carrousel</button>
+    wp_enqueue_script(
+        'js_carrousel',
+        plugin_dir_url(__FILE__) . 'js/carrousel.js',
+        array(),
+        $version_js,
+        true // ajoute le script carrousel.js à la fin de la page
+    );
+}
+add_action('wp_enqueue_scripts', 'enfiler_script_css');
+
+function genere_boite()
+{
+    return '<button class="carrousel__ouvrir">Ouvrir Carrousel</button>
                 <div class="carrousel">
                     <button class="carrousel__x">X</button>
+                    <div class="btnPrecSuiv">
+                    <button class="precedent">&lt;</button>
+                    <button class="suivant">&gt;</button>
+                </div>
                     <figure class="carrousel__figure"></figure>
                     <form class="carrousel__form"></form>
                 </div>';
-    }
-    add_shortcode('carrousel', 'genere_boite');
-
-?>
+}
+add_shortcode('carrousel', 'genere_boite');
